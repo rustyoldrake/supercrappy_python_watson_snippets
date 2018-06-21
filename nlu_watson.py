3c3f7a600b632278445d5526ae4f6df9afa2ddd8
Ryan Anderson <rustyoldrake@gmail.com>
2018-06-20 18:15:52 -0700

    erroring so pulled fresh from master with from __future__ import print_function

--- nlu_watson.py
+++ nlu_watson.py
@@ -1,18 +1,17 @@
+from __future__ import print_function
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
-import watson_developer_cloud.natural_language_understanding.features.v1 as \
-    features
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
-# you need to go to bluemix - https://console.ng.bluemix.net/ sign up for free, and then stand up a free Watson NLU endpoint to get your own keys
+
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2017-02-27',
-    username='b26d50dd-xxxx-xxxx-xxxx-c965f958b228',
-    password='xxxxxxxxxxx')
-
+    username='XXXXXXXXXXXXXX',
+    password='XXXXXXXXXXXXXX')
 
 response = natural_language_understanding.analyze(
     text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
          'Superman fears not Banner, but Wayne.',
-    features=[features.Entities(), features.Keywords()])
+    features=Features(entities=EntitiesOptions(), keywords=KeywordsOptions()))
 
 print(json.dumps(response, indent=2))
